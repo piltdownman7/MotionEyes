0128ed873166f156165dc68001ffb05be7c9361d
Brett Graham <brett.graham@quickmobile.com>
2015-10-26 17:38:00 -0700

    Licensing

--- MotionEyes/ViewController.m
+++ MotionEyes/ViewController.m
@@ -4,6 +4,13 @@
 //
 //  Created by Brett Graham on 2015-10-10.
 //  Copyright © 2015 Brett Graham. All rights reserved.
+//  ----------------------------------------------------------------------------
+//  THE BEER-WARE LICENSE" (Revision 42):
+//  <brett.s.graham@gmail.com> wrote this file.  As long as you retain this notice you
+//  can do whatever you want with this stuff. If we meet some day, and you think
+//  this stuff is worth it, you can buy me a beer in return.   Brett Graham
+//  ----------------------------------------------------------------------------
+//
 //
 #import "EyesView.h"
 #import "ViewController.h"
